72b627c77ac5e33f963440cde44ce224076bec06
Satabdi-stack <84120031+Satabdi-stack@users.noreply.github.com>
2021-05-23 18:35:26 +0530

    Create Withdrawal.java

---
+++ Daily assignments/Day 5/CaseStudy 2/Withdrawal.java
@@ -0,0 +1,20 @@
+package casestudy4;
+
+public class Withdrawal extends Transaction {
+    private double amount;
+
+//    Constructor
+    public Withdrawal(int accNum, double amt) {
+        super(accNum);
+        this.amount = amt;
+    }
+
+
+//    Methods
+    public void execute(double balance) {
+        super.execute();
+        balance -= amount;
+        System.out.println("Balance: " + balance);
+    }
+}
+
