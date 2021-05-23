69781f06e6fbed75ab236d659031aee5e219e6af
Satabdi-stack <84120031+Satabdi-stack@users.noreply.github.com>
2021-05-23 18:26:53 +0530

    Add files via upload

---
+++ Daily assignments/Day 5/CaseStudy 1/Academic.java
@@ -0,0 +1,17 @@
+package casestudy3;
+public class Academic extends PayRoll {
+    private String name;
+    private int salary;
+
+    Academic() {}
+
+    public Academic(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+        super.adjustSalary(salary);
+    }
+
+    public void giveLecture() {
+        System.out.println("Giving lecture");
+    }
+}
---
+++ Daily assignments/Day 5/CaseStudy 1/Admin.java
@@ -0,0 +1,18 @@
+package casestudy3;
+
+public class Admin extends PayRoll {
+    private String name;
+    private int salary;
+
+    Admin() {}
+
+    public Admin(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+        super.adjustSalary(salary);
+    }
+
+    public void doAdminStuff() {
+        System.out.println("Doing admin stuff");
+    }
+}
