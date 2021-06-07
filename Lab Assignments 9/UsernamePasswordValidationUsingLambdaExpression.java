7b2253105cd2f31ecc30e12f4186ad4fc97dc94f
Satabdi-stack <84120031+Satabdi-stack@users.noreply.github.com>
2021-06-07 11:29:50 +0530

    Create UsernamePasswordValidationUsingLambdaExpression.java

---
+++ Lab Assignments 9/UsernamePasswordValidationUsingLambdaExpression.java
@@ -0,0 +1,8 @@
+package lab9;
+
+@FunctionalInterface
+public interface UsernamePasswordValidationUsingLambdaExpression {
+
+	//boolean method to validate username & password
+	boolean validateusername_password(String username,String password);
+}
