2b87ce068c3ab5fb9570175c6907bba93cfd929a
Satabdi-stack <84120031+Satabdi-stack@users.noreply.github.com>
2021-06-07 11:36:44 +0530

    Update UsernamePasswordValidationMain.java

--- Lab Assignments 9/UsernamePasswordValidationMain.java
+++ Lab Assignments 9/UsernamePasswordValidationMain.java
@@ -1,5 +1,25 @@
 package lab9;
 
+import java.util.Scanner;
+
+
 public class UsernamePasswordValidationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		UsernamePasswordValidationUsingLambdaExpression upv = (username,password)->{
+			return (username.equals("genesis") && password.equals("genesis1234"));
+		};
+		
+		System.out.print("Enter the username : ");
+		String username=sc.next();
+		
+		System.out.print("Enter the password : ");
+		String password=sc.next();
+		
+		System.out.println("\nAuthentication Status : "+upv.validateusername_password(username, password));
+		sc.close();
+	}
 
 }
